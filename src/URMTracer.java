4347bda13b32ecc1dd9345f10ed2d18c1f29f9d0
rjesteban <ajcp.esteban@gmail.com>
2016-03-08 22:52:23 +0800

    modified code

--- src/URMTracer.java
+++ src/URMTracer.java
@@ -5,22 +5,38 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/*
+ * URM Tracer
+ * I hereby solemnly swear that I did code this by myself.
+ */
+/**
+ *
+ * @author 2013-50109 Esteban, Arnold Joseph Caesar P.
+ * @version 1.0
+ * @since 2016-3-6
+ */
 public class URMTracer {
 
-    private int[] register;
+    private int[] register; //the values of the register from input file
     private int pointer;
-    private ArrayList<String[]> code;
-    private ArrayList<String> output;
-    private File source;
+    private ArrayList<String[]> code; //contains the code from input file
+    private ArrayList<String> output; // contains the output upon execution of code
+    private File source; //input file
 
     public URMTracer(File source) {
         this.register = new int[10];
-        this.pointer = 1;
+        this.pointer = 1; //code execution starts at line 1
         this.code = new ArrayList<String[]>();
         this.output = new ArrayList<String>();
         this.source = source;
     }
-
+    
+    /**
+     * loadValues stores all values to the attributes of the class
+     * codes are split into string arrays, as delimited by spaces
+     * add null code to signify end of execution
+     * @throws FileNotFoundException scanner's exception if input is not given
+     */
     public void loadValues() throws FileNotFoundException {
         Scanner sc = new Scanner(source);
         this.code.add(null);
@@ -36,9 +52,14 @@ public class URMTracer {
         }
         this.code.add(null);
     }
-
+    
+    /**
+     * interpret the code by printing the values of the register whenever a
+     * line of code is executed
+     * @throws IOException
+     */
     public void interpret() throws IOException {
-        this.printRegisters();
+        this.storeRegistersToOutput();
 
         for (; this.pointer != this.code.size() + 1; this.pointer++) {
 
@@ -64,7 +85,7 @@ public class URMTracer {
                             pointer = Integer.parseInt(lineCode[3]) - 1;
                         }   break;
                 }
-                this.printRegisters();
+                this.storeRegistersToOutput();
             } catch (NumberFormatException |
                     NullPointerException exception) {
                 //end of interpretation
@@ -75,7 +96,12 @@ public class URMTracer {
         }
 
     }
-
+    
+    /**
+     * printRegisters prints the current value of the registers, 
+     * separated by space
+     * 
+     */
     private void printRegisters() {
         String s = new String();
         for (int i = 0; i < this.register.length - 1; i++) {
@@ -84,9 +110,28 @@ public class URMTracer {
         }
         System.out.println(this.register[9]);
         s += this.register[9] + "\n";
-        this.output.add(s);
     }
 
+    /**
+     * printRegisters prints the current value of the registers, 
+     * separated by space
+     * 
+     */
+    private void storeRegistersToOutput() {
+        String s = new String();
+        for (int i = 0; i < this.register.length - 1; i++) {
+            s += this.register[i] + " ";
+        }
+        s += this.register[9] + "\n";
+        this.output.add(s);
+    }
+    
+    
+    /**
+     * writeRegisters writes the output to the file upon every line execution
+     * output is the value of the register upon line execution
+     * @throws IOException
+     */
     private void writeRegisters() throws IOException {
         String fileName = source.getName().split("[.]")[0] + ".out";
         FileWriter fw;
@@ -97,6 +142,12 @@ public class URMTracer {
 
         fw.close();
     }
+
+        
+    /**
+     * clear clears code, removes source, sets pointer back to start and
+     * register all to zero
+     */
 
     public void clear() {
         this.code.clear();
